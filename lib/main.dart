0eb327e9f913c146777bc07210fa119a2d906ea5
sxmdln <sdelasnieves@gbox.adnu.edu.ph>
2023-11-19 02:04:37 +0800

    Added an appBar, a theme, and placeholder for user profile.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
-import 'package:a_check_web/welcome.dart';
+import 'package:a_check_web/splash.dart';
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MainApp()));
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'Inter',
+        useMaterial3:false,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+          primary: Colors.white,
+          secondary: Colors.green,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: Colors.green),
+        ),
+      ),
+      home: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,6 +23,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Welcome());
+    return Scaffold(
+      backgroundColor: const Color(0xff000000),
+      appBar: AppBar(
+        elevation: 0.5,
+        title:  Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 0),
+              child: Image(
+                image: AssetImage("assets/images/logo.png"), height: 55),
+              ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                const Column(
+                  children: [
+                    Text("De La Cruz, John",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                    ),
+                    Text("Ateneo De Naga University",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+
+                IconButton(
+                  //TODO: DropdownMenu (if logged in) with items[View Profile, Settings, Logout] or (if not logged in) items[Sign In, Register]
+                  color: Colors.black,
+                  icon: const Icon(Icons.arrow_drop_down, size: 25),
+                  tooltip: 'Profile',
+                  onPressed: (){},
+                ),
+              ]
+            ),
+          ],
+        ),
+      ),
+      body: const Splash(),
+    );
   }
 }

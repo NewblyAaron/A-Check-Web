3bde76afc859e462934e4757b96aef18b941938c
sxmdln <sdelasnieves@gbox.adnu.edu.ph>
2023-11-19 02:02:32 +0800

    Added new pages with their respective lists (forms still empty tho), and organized them in folders

---
+++ lib/forms/student_form_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudentFormPage extends StatefulWidget {
+  const StudentFormPage({super.key});
+
+  @override
+  State<StudentFormPage> createState() => _StudentFormPageState();
+}
+
+class _StudentFormPageState extends State<StudentFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/forms/teacher_form_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeacherFormPage extends StatefulWidget {
+  const TeacherFormPage({super.key});
+
+  @override
+  State<TeacherFormPage> createState() => _TeacherFormPageState();
+}
+
+class _TeacherFormPageState extends State<TeacherFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

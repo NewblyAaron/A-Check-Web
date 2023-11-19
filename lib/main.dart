63bef9990dba22bbbf9306ff98204eaf7c9ae382
Aaron Serrano <amserrano1337@gmail.com>
2023-11-19 21:47:07 +0800

    initialize firebase before running app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:a_check_web/firebase_options.dart';
 import 'package:a_check_web/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
       theme: ThemeData(fontFamily: 'Inter',
         useMaterial3:false,

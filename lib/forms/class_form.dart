73773b2ae56b2a3c6a705f54f7c0d8314a274f79
sxmdln <57649092+sxmdln@users.noreply.github.com>
2023-11-29 00:45:01 +0800

    Changed UI for Class Forms

--- lib/forms/class_form.dart
+++ lib/forms/class_form.dart
@@ -1,10 +1,13 @@
 import 'package:a_check_web/forms/class_form_con.dart';
 import 'package:a_check_web/utils/abstracts.dart';
 import 'package:a_check_web/widgets/schedule_row.dart';
+import 'package:a_check_web/model/school_class.dart';
 import 'package:flutter/material.dart';
 
 class ClassForm extends StatefulWidget {
-  const ClassForm({super.key});
+  const ClassForm({super.key, this.schoolClass});
+
+  final SchoolClass? schoolClass;
 
   @override
   State<ClassForm> createState() => ClassFormState();
@@ -16,37 +19,48 @@ class ClassFormView extends WidgetView<ClassForm, ClassFormState> {
   Widget buildScheduleList() {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text("Schedule",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-            TextButton(
-              onPressed: state.addSchedule,
-              style: TextButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+        SizedBox(
+          width: 600,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text("Schedule",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                TextButton(
+                  onPressed: state.addSchedule,
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'Add Schedule',
+                    style: TextStyle(color: Colors.green),
+                  ),
                 ),
-              ),
-              child: const Text(
-                'Add Schedule',
-                style: TextStyle(color: Colors.green),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
-        ListView(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            children: state.schedules
-                .map((e) => ScheduleRow(
-                      schedule: e,
-                      index: state.schedules.indexOf(e),
-                      onEdit: state.editSchedule,
-                      onDelete: state.deleteSchedule,
-                    ))
-                .toList())
+        SizedBox(
+          width: 545,
+          child: ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              children: state.schedules
+                  .map((e) => ScheduleRow(
+                        schedule: e,
+                        index: state.schedules.indexOf(e),
+                        onEdit: state.editSchedule,
+                        onDelete: state.deleteSchedule,
+                      ))
+                  .toList()),
+        )
       ],
     );
   }
@@ -55,68 +69,109 @@ class ClassFormView extends WidgetView<ClassForm, ClassFormState> {
     return Form(
       key: state.formKey,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          const Text("Add Class",
-              textAlign: TextAlign.start,
-              overflow: TextOverflow.clip,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.normal,
-                fontSize: 50,
-                color: Color(0xff000000),
-              )),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: TextFormField(
-              //controller: state.idCon,
-              //validator: Validators.hasValue,
-              obscureText: false,
-              textAlign: TextAlign.start,
-              textInputAction: TextInputAction.next,
-              maxLines: 1,
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 14,
-                color: Colors.black54,
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 32, 0, 0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8, 16,8,16),
+                    child: Text("Add Class",
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 35,
+                          color: Color(0xff000000),
+                        )),
+                  ),
+                ],
               ),
-              decoration: const InputDecoration(labelText: "Code"),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: TextFormField(
-              // controller: state.fNameCon,
-              // validator: Validators.hasValue,
-              obscureText: false,
-              textAlign: TextAlign.start,
-              textInputAction: TextInputAction.next,
-              maxLines: 1,
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 14,
-                color: Colors.black54,
+          SizedBox(
+            width: 600,
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                //controller: state.idCon,
+                //validator: Validators.hasValue,
+                obscureText: false,
+                textAlign: TextAlign.start,
+                textInputAction: TextInputAction.next,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  hintText: 'e.g. MTH101',
+                  labelText: "Class Code"
+                ),
               ),
-              decoration: const InputDecoration(labelText: "Name"),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: TextFormField(
-              // controller: state.mNameCon,
-              // validator: Validators.hasValue,
-              obscureText: false,
-              textAlign: TextAlign.start,
-              textInputAction: TextInputAction.next,
-              maxLines: 1,
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 14,
-                color: Colors.black54,
+          SizedBox(
+            width: 600,
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                // controller: state.fNameCon,
+                // validator: Validators.hasValue,
+                obscureText: false,
+                textAlign: TextAlign.start,
+                textInputAction: TextInputAction.next,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    hintText: 'e.g. Mathematics in the Modern World',
+                    labelText: "Class Name"
+                ),
               ),
-              decoration: const InputDecoration(labelText: "Section"),
+            ),
+          ),
+          SizedBox(
+            width: 600,
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                // controller: state.mNameCon,
+                // validator: Validators.hasValue,
+                obscureText: false,
+                textAlign: TextAlign.start,
+                textInputAction: TextInputAction.next,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 14,
+                  color: Colors.black54,
+                ),
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    hintText: 'e.g. ZT21',
+                    labelText: "Class Section"
+                ),
+              ),
             ),
           ),
         ],
@@ -126,23 +181,67 @@ class ClassFormView extends WidgetView<ClassForm, ClassFormState> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(100, 50, 100, 0),
-      child: Align(
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            buildClassInfo(),
-            buildScheduleList(),
-            ElevatedButton(
-              onPressed: () {}, //state.finalize,
-              child: const Text(
-                "Confirm",
-                style: TextStyle(color: Colors.black),
+    return SizedBox(
+      width: 600,
+      child: Column(
+        children: [
+          buildClassInfo(),
+          buildScheduleList(),
+          const Spacer(flex: 1),
+          Row(
+            children: [
+              Material(
+                color: Colors.grey.shade200,
+                child: InkWell(
+                  hoverColor: Colors.red.withOpacity(0.4),
+                  highlightColor: Colors.red.withOpacity(0.4),
+                  splashColor: Colors.red.withOpacity(0.5),
+                  onTap:  () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    width:300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(35),
+                      // adding color will hide the splash effect
+                      // color: Colors.blueGrey.shade200,
+                    ),
+                    child: const Text("Cancel", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),),
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+              Material(
+                color: Colors.lightGreen.shade200,
+                child: InkWell(
+                  hoverColor: Colors.green.withOpacity(0.4),
+                  highlightColor: Colors.green.withOpacity(0.4),
+                  splashColor: Colors.green.withOpacity(0.5),
+                  onTap: (){},  //state.finalize
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    width:300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(35),
+                      // adding color will hide the splash effect
+                      // color: Colors.blueGrey.shade200,
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("Confirm", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500), ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

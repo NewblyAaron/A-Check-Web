99fbba73fee1a82fd4bb385eb79cc4d86e21b5ce
sxmdln <57649092+sxmdln@users.noreply.github.com>
2023-12-09 01:53:50 +0800

    changed button (customborder) fix when clicking

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -238,6 +238,9 @@ class RegisterPageView extends WidgetView<RegisterPage, RegisterPageState> {
             ),
             const SizedBox(height:12),
             InkWell(
+              customBorder: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
               // hoverColor: const Color(0xff153faa).withOpacity(0.8),
               // highlightColor: const Color(0xff153faa).withOpacity(0.4),
               // splashColor: const Color(0xff153faa).withOpacity(1),

312bae19121fcbe262a8f142ca8da87a1e1e8818
Aaron Serrano <amserrano1337@gmail.com>
2023-12-11 22:02:00 +0800

    remove unused packages in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:a_check_web/auth.dart';
 import 'package:a_check_web/firebase_options.dart';
 import 'package:a_check_web/globals.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:google_fonts/google_fonts.dart';
